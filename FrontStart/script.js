16607812d1bcfe4cbf53fc5bc4a956fd161798dc
GarciaGabriell <163200463+GarciaGabriell@users.noreply.github.com>
2025-06-02 12:55:21 -0300

    Add files via upload

---
+++ FrontStart/script.js
@@ -0,0 +1,88 @@
+function Baixar1() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_1.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_1.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar2() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_2.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_2.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar3() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_3.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_3.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar4() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_4.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_4.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar5() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_5.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_5.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar6() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_6.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_6.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+  function Baixar7() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_7.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_7.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar8() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_8.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_8.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+  function Baixar9() {
+  const link = document.createElement("a");
+  link.href = "Complementares/aula_9.zip";       // Caminho para o seu arquivo .zip
+  link.download = "aula_9.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
+
+function BaixarTodos() {
+  const link = document.createElement("a");
+  link.href = "Complementares/TodosExercicios.zip";       // Caminho para o seu arquivo .zip
+  link.download = "TodosExercicios.zip";   // Nome do arquivo ao baixar (pode personalizar)
+  document.body.appendChild(link);          // Adiciona o link ao DOM
+  link.click();                             // Dispara o clique
+  document.body.removeChild(link);          // Remove o link do DOM
+}
